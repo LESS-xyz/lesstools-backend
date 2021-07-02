fac983614561c0c93a028284f52ae676e88f10ec
dsahdr <3180Gmtu>
2021-07-02 17:30:23 +0300

    rates fixes

--- lesstools/consts.py
+++ lesstools/consts.py
@@ -1,8 +1,11 @@
 MAX_DIGITS = len(str(2 ** 256))
 
+# TODO move decimals to rates model and check decimals from contract
 DECIMALS = {
+    'USD': 10 ** 2,
     'USDC': 10 ** 6,
     'ethereum': 10 ** 18,
     'binance-smart-chain': 10 ** 18,
+    'WBNB': 10 ** 18,
 
 }
--- lesstools/rates/api.py
+++ lesstools/rates/api.py
@@ -18,8 +18,10 @@ def calculate_amount(original_amount, from_currency, to_currency='USD'):
         flush=True)
 
     usd_rates = get_usd_prices()
-
-    currency_rate = usd_rates[from_currency] / usd_rates[to_currency]
-    amount = int(int(original_amount / DECIMALS[from_currency] * DECIMALS[to_currency]) * currency_rate)
+    if to_currency == 'USD':
+        currency_rate = usd_rates[from_currency]
+    else:
+        currency_rate = usd_rates[from_currency] / usd_rates[to_currency]
+    amount = int(int(original_amount) / DECIMALS[from_currency] * DECIMALS[to_currency] * float(currency_rate))
 
     return amount, currency_rate
