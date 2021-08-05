0950acf0ddeeee7fc98e2d1bdca632452a33caf2
dsahdr <3180Gmtu>
2021-08-05 11:37:01 +0300

    lost upload

---
+++ lesstools/networks/models.py
@@ -0,0 +1,25 @@
+from web3 import Web3
+from django.db import models
+from web3.middleware import geth_poa_middleware
+
+
+class Network(models.Model):
+    name = models.CharField(max_length=50)
+    endpoint = models.CharField(max_length=200)
+    last_processed_block = models.IntegerField(default=1)
+    needs_poa_middleware = models.BooleanField(default=False)
+    native_api_url = models.CharField(max_length=400)
+    token_api_url = models.CharField(max_length=400)
+    native_decimals = models.IntegerField(default=18)
+
+    def get_web3_connection(self):
+        web3 = Web3(Web3.HTTPProvider(self.endpoint))
+        if self.needs_poa_middleware:
+            web3.middleware_onion.inject(geth_poa_middleware, layer=0)
+        return web3
+
+
+class Token(models.Model):
+    network = models.ForeignKey('Network', on_delete=models.CASCADE)
+    rate = models.ForeignKey('rates.UsdRate', on_delete=models.CASCADE)
+    address = models.CharField(max_length=128)
---
+++ lesstools/rates/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+from lesstools.consts import MAX_DIGITS
+
+
+class UsdRate(models.Model):
+    """
+    Model that store coingecko token rates and info for coingecko requests
+    """
+    currency = models.CharField(max_length=100)
+    rate = models.DecimalField(max_digits=MAX_DIGITS, decimal_places=2, null=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.coin.name
