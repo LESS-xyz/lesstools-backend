a506862abb08c8264266de0ac5a9d8e987d72e5f
Arsenii Poriadin <ar.poryadin@yandex.ru>
2021-08-20 18:59:40 +0300

    bugfix: usd amount calculation

--- lesstools/rates/api.py
+++ lesstools/rates/api.py
@@ -19,6 +19,7 @@ def calculate_amount(original_amount, from_currency, to_currency='USD'):
         currency_rate = usd_rates[from_currency]
     else:
         currency_rate = usd_rates[from_currency] / usd_rates[to_currency]
-    amount = int(int(original_amount) / DECIMALS[from_currency] * DECIMALS[to_currency] * float(currency_rate))
+    # it seems that multiplying by DECIMALS[to_currency] shouldn't be used here
+    amount = int(int(original_amount) / DECIMALS[from_currency] * float(currency_rate))
 
     return amount, currency_rate
