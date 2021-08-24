9973ae4230ee9b37c47ba04e9a04e7df23997244
Arsenii Poriadin <ar.poryadin@yandex.ru>
2021-08-24 19:38:29 +0300

    stepped downgrade of user plans (if there was more than active payment)

--- lesstools/accounts/admin.py
+++ lesstools/accounts/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from lesstools.accounts import models
 
+
+class PlanPaymentAdmin(admin.ModelAdmin):
+    readonly_fields = ('payment_time', )
+
+
 admin.site.register(models.PlanPrice)
 admin.site.register(models.AdvUser)
-admin.site.register(models.PlanPayment)
+admin.site.register(models.PlanPayment, PlanPaymentAdmin)
--- lesstools/accounts/tasks.py
+++ lesstools/accounts/tasks.py
@@ -7,11 +7,18 @@ import logging
 @dramatiq.actor(max_retries=0)
 def check_paid():
     logging.info('starting paid check')
+
     for user in AdvUser.objects.exclude(plan=AdvUser.Plans.FREE):
         # for every user with active plan check if payment end time is expired and downgrade him
-        # todo check if there're another payments from him and probably downgrade by one tier below, not FREE right away
-        if not user.payments.filter(end_time__gte=timezone.now()):
+        active_payments = user.payments.filter(end_time__gte=timezone.now()).count()
+        if active_payments == 0:
             user.plan = AdvUser.Plans.FREE
-            user.save()
-            logging.info(f'payment of {user.username} is expired, downgrading')
+        elif active_payments == 1:
+            user.plan = AdvUser.Plans.STANDARD
+        else:
+            continue
+
+        user.save()
+        logging.info(f'payment of {user.username} is expired, downgrading to {user.plan} plan')
+
     logging.info('ending paid check')
